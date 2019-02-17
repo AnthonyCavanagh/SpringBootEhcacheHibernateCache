8c7e3f1484e7d948398cc07a2101860466ee05e9
Anthony Cavanagh <cavanaghtony@hotmail.com>
2019-02-17 15:37:36 +0000

    First commit

---
+++ src/main/java/com/cav/spring/service/bank/cache/EHCacheConfig.java
@@ -0,0 +1,30 @@
+package com.cav.spring.service.bank.cache;
+
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@EnableCaching
+@Configuration
+public class EHCacheConfig {
+
+	@Bean
+    public EhCacheCacheManager ehCacheCacheManager() {
+        EhCacheCacheManager cacheManager = new EhCacheCacheManager();
+        cacheManager.setCacheManager(ehCacheManagerFactory().getObject());
+        cacheManager.setTransactionAware(true);
+        return cacheManager;
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheManagerFactory(){
+        EhCacheManagerFactoryBean ehCacheBean = new EhCacheManagerFactoryBean();
+        ehCacheBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
+        ehCacheBean.setShared(true);
+        return ehCacheBean;
+    }
+}
---
+++ src/main/java/com/cav/spring/service/bank/service/FundServiceImpl.java
@@ -0,0 +1,63 @@
+package com.cav.spring.service.bank.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cav.spring.service.bank.controller.BankServiceController;
+import com.cav.spring.service.bank.entity.Fund;
+import com.cav.spring.service.bank.model.bank.BankId;
+import com.cav.spring.service.bank.model.fund.FundId;
+import com.cav.spring.service.bank.model.fund.FundPOJO;
+import com.cav.spring.service.bank.model.fund.Funds;
+import com.cav.spring.service.bank.repository.FundRepository;
+
+@Service
+public class FundServiceImpl extends MappEntitiesAbstract implements FundService {
+	
+	@Autowired
+	FundRepository repository;
+	
+	private static final Logger logger = LoggerFactory.getLogger(FundServiceImpl.class);	
+
+	@Override
+	public void updateFunds(Funds funds) {
+		List<Fund> entities = new ArrayList<Fund>();
+		List<FundPOJO> fundList = funds.getFundList();
+		for(FundPOJO fund : fundList){
+			Fund fundEntity = repository.findOne(fund.getFundId());
+			if(fundEntity != null){
+				entities.add(updateFundEntity(fund, fundEntity));
+			} else {
+				//error
+			}
+		}
+		repository.save(entities);
+	}
+
+	@Override
+	/**
+	 * Have to use repository.findOne, to load an entity into the second cache
+	 */
+	public Funds listFunds(List<FundId> fundIds) {
+		List<Fund> funds = new ArrayList<Fund>();
+		for(FundId fundId : fundIds){
+			logger.debug("Extracting fund from repo "+fundId.getId());
+			funds.add(repository.findOne(fundId.getId()));
+		}
+		return mapFunds(funds);
+	}
+
+	@Override
+	public void removeFunds(List<FundId> fundIds) {
+		for(FundId fundId : fundIds){
+			repository.delete(fundId.getId());
+		}
+	}
+	
+}
